1020a2556678b4d7e2f53435987292a031a87c10
Oleg Subachev <Oleg.Subachev@aspose.com>
2022-08-19 05:39:48 -0500

    update to 22.8.0

--- api_client.go
+++ api_client.go
@@ -176,7 +176,7 @@ func (c *APIClient) prepareRequest (
     
     // set custom header
     headerParams["x-aspose-client"] = "go sdk"
-    headerParams["x-aspose-client-version"] = "22.7.0"
+    headerParams["x-aspose-client-version"] = "22.8.0"
 
     // Detect postBody type and post.
     if postBody != nil {
