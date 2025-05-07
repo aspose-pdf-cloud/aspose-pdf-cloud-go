09c8ac9a06e597a4a01481b61c1631c7326b9c74
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-05-07 07:40:29 +0300

    Add files via upload

---
+++ uses_cases/bookmarks/replace_bookmark.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+
+	asposepdfcloud "github.com/aspose-pdf-cloud/aspose-pdf-cloud-go/v25"
+)
+
+func replaceBookmark(pdf_api *asposepdfcloud.PdfApiService, document_name string, bookmark_path string, title string, remote_folder string) {
+	args := map[string]interface{}{
+		"folder": remote_folder,
+	}
+
+	bookmark := asposepdfcloud.Bookmark{
+		Action:            "GoTo",
+		Bold:              true,
+		Italic:            false,
+		Title:             title,
+		PageDisplay:       "XYZ",
+		PageDisplayBottom: 10,
+		PageDisplayLeft:   10,
+		PageDisplayRight:  10,
+		PageDisplayTop:    10,
+		PageDisplayZoom:   2,
+		PageNumber:        1,
+		Color:             &asposepdfcloud.Color{A: 0x00, R: 0x00, G: 0xFF, B: 0x00},
+	}
+
+	result, httpResponse, err := pdf_api.PutBookmark(document_name, bookmark_path, bookmark, args)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else if httpResponse.StatusCode < 200 || httpResponse.StatusCode > 299 {
+		fmt.Println("Unexpected error!")
+	} else {
+		fmt.Println(result.Bookmark)
+	}
+}
