da0668dc44b7d72daf10f5c0ddc55cd3e71ea8f9
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-05-07 07:37:08 +0300

    Update append_bookmark.go

--- uses_cases/bookmarks/append_bookmark.go
+++ uses_cases/bookmarks/append_bookmark.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"fmt"
+
 	asposepdfcloud "github.com/aspose-pdf-cloud/aspose-pdf-cloud-go/v25"
 )
 
-func appendBookmark(pdf_api *asposepdfcloud.PdfApiService, document_name string, title string, remote_folder string) {
-	bookmarkPath := "1"
+func appendBookmark(pdf_api *asposepdfcloud.PdfApiService, document_name string, bookmark_path string, title string, remote_folder string) {
 	args := map[string]interface{}{
 		"folder": remote_folder,
 	}
@@ -24,5 +25,13 @@ func appendBookmark(pdf_api *asposepdfcloud.PdfApiService, document_name string,
 		PageNumber:        1,
 		Color:             &asposepdfcloud.Color{A: 0x00, R: 0x00, G: 0xFF, B: 0x00},
 	}
-	_, _, _ = pdf_api.PostBookmark(document_name, bookmarkPath, []asposepdfcloud.Bookmark{bookmark}, args)
+
+	result, httpResponse, err := pdf_api.PostBookmark(document_name, bookmark_path, []asposepdfcloud.Bookmark{bookmark}, args)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else if httpResponse.StatusCode < 200 || httpResponse.StatusCode > 299 {
+		fmt.Println("Unexpected error!")
+	} else {
+		fmt.Println(result.Bookmarks)
+	}
 }
