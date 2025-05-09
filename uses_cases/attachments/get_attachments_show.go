989c082704950d29a185ebc6403d290a12993210
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-05-09 11:41:12 +0300

    PDFCLOUD-4849: added snippets for Attachments

---
+++ uses_cases/attachments/get_attachments_show.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+
+	asposepdfcloud "github.com/aspose-pdf-cloud/aspose-pdf-cloud-go/v25"
+)
+
+func getDocumentAttachments(pdf_api *asposepdfcloud.PdfApiService, document_name string, remote_folder string) {
+	args := map[string]interface{}{
+		"folder": remote_folder,
+	}
+	result, httpResponse, err := pdf_api.GetDocumentAttachments(document_name, args)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else if httpResponse.StatusCode < 200 || httpResponse.StatusCode > 299 {
+		fmt.Println("Unexpected error!")
+	} else {
+		fmt.Println(result.Attachments.Links)
+	}
+}
