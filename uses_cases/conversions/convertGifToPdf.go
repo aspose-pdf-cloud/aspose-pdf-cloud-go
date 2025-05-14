f5583e0f541ed2087080bc282e9d3f673314950a
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-05-14 18:55:01 +0300

    PDFCLOUD-4856: added snippets for Conversions

---
+++ uses_cases/conversions/convertGifToPdf.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"path"
+
+	asposepdfcloud "github.com/aspose-pdf-cloud/aspose-pdf-cloud-go/v25"
+)
+
+func converGifToPdf(pdf_api *asposepdfcloud.PdfApiService, gif_file string, pdf_name string, remote_folder string) {
+	imageTemplateList := []asposepdfcloud.ImageTemplate{}
+
+	imageTemplate := asposepdfcloud.ImageTemplate{
+		ImagePath:    path.Join(remote_folder, gif_file),
+		ImageSrcType: asposepdfcloud.ImageSrcTypeCommon,
+	}
+	imageTemplateList = append(imageTemplateList, imageTemplate)
+
+	uploadFile(pdf_api, gif_file)
+
+	imageTemplatesRequest := asposepdfcloud.ImageTemplatesRequest{
+		IsOCR:      true,
+		OCRLangs:   "eng",
+		ImagesList: imageTemplateList,
+	}
+
+	args := map[string]interface{}{
+		"folder": remote_folder,
+	}
+
+	result, httpResponse, err := pdf_api.PutImageInStorageToPdf(pdf_name, imageTemplatesRequest, args)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else if httpResponse.StatusCode < 200 || httpResponse.StatusCode > 299 {
+		fmt.Println("Unexpected error!")
+	} else {
+		fmt.Println(result)
+	}
+}
---
+++ uses_cases/conversions/convertPclToPdf.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"path"
+
+	asposepdfcloud "github.com/aspose-pdf-cloud/aspose-pdf-cloud-go/v25"
+)
+
+func convertPclToPdf(pdf_api *asposepdfcloud.PdfApiService, pcl_document string, pdf_name string, remote_folder string) {
+	args := map[string]interface{}{
+		"folder": remote_folder,
+	}
+
+	src_path := path.Join(remote_folder, pcl_document)
+
+	result, httpResponse, err := pdf_api.PutPclInStorageToPdf(pdf_name, src_path, args)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	} else if httpResponse.StatusCode < 200 || httpResponse.StatusCode > 299 {
+		fmt.Println("Unexpected error!")
+	} else {
+		fmt.Println(result)
+	}
+}
