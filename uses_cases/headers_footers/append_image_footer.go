c8483c96477a2165b0532351ce77f93987203bee
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-05-20 10:02:10 +0300

    PDFCLOUD-4864: added snippets for Headers/Footers

---
+++ uses_cases/headers_footers/append_image_footer.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"path"
+
+	asposepdfcloud "github.com/aspose-pdf-cloud/aspose-pdf-cloud-go/v25"
+)
+
+func appendImageFooter(pdf_api *asposepdfcloud.PdfApiService, document_name string, image_name string, startPage int32, end_page int32, remote_folder string) {
+	uploadFile(pdf_api, image_name)
+
+	args := map[string]interface{}{
+		"folder":          remote_folder,
+		"startPageNumber": startPage,
+		"endPageNumber":   end_page,
+	}
+
+	footer := asposepdfcloud.ImageFooter{
+		Background:          true,
+		LeftMargin:          1,
+		RightMargin:         2,
+		HorizontalAlignment: asposepdfcloud.HorizontalAlignmentCenter,
+		Opacity:             1,
+		Rotate:              asposepdfcloud.RotationNone,
+		RotateAngle:         0,
+		XIndent:             0,
+		YIndent:             0,
+		Zoom:                1,
+		Width:               24,
+		Height:              24,
+		FileName:            path.Join(remote_folder, image_name),
+	}
+
+	result, httpResponse, err := pdf_api.PostDocumentImageFooter(document_name, footer, args)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else if httpResponse.StatusCode < 200 || httpResponse.StatusCode > 299 {
+		fmt.Println("Unexpected error!")
+	} else {
+		fmt.Println(result)
+	}
+}
