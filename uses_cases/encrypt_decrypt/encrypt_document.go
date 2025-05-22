26a64c9ed184cd95436d8ea424a34a7c14ce3f28
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-05-22 09:41:31 +0300

    PDFCLOUD-4865: added snippets for Encrypt-Decrypt

---
+++ uses_cases/encrypt_decrypt/encrypt_document.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+
+	asposepdfcloud "github.com/aspose-pdf-cloud/aspose-pdf-cloud-go/v25"
+)
+
+func encryptDocument(pdf_api *asposepdfcloud.PdfApiService, document string, output_document string, userPassword string, ownerPassword string, encrypt_algorithm string, remote_folder string) {
+	uploadFile(pdf_api, document)
+
+	args := map[string]interface{}{
+		"folder": remote_folder,
+	}
+
+	result, httpResponse, err := pdf_api.PostEncryptDocumentInStorage(document,
+		base64.StdEncoding.EncodeToString([]byte(userPassword)),
+		base64.StdEncoding.EncodeToString([]byte(ownerPassword)),
+		encrypt_algorithm,
+		args)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else if httpResponse.StatusCode < 200 || httpResponse.StatusCode > 299 {
+		fmt.Println("Unexpected error!")
+	} else {
+		fmt.Println(result)
+
+		downloadFile(pdf_api, document, output_document)
+	}
+}
