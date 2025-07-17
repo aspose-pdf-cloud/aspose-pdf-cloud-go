15c4822ec682986a7f916536add29aaef4a7d900
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-07-18 01:08:06 +0300

    Update get_annotation_by_id.go

--- uses_cases/annotations/get_annotation_by_id.go
+++ uses_cases/annotations/get_annotation_by_id.go
@@ -7,7 +7,7 @@ import (
 )
 
 func RequestAnnotationById(pdf_api *asposepdfcloud.PdfApiService, document_name string, annotation_id string, remote_folder string) {
-	// Get annotation from the page in the PDF document.
+	// Get annotation by Id in the PDF document.
 	UploadFile(pdf_api, document_name)
 	args := map[string]interface{}{
 		"folder": remote_folder,
@@ -16,7 +16,7 @@ func RequestAnnotationById(pdf_api *asposepdfcloud.PdfApiService, document_name 
 	if err != nil {
 		fmt.Println(err.Error())
 	} else if httpResponse.StatusCode < 200 || httpResponse.StatusCode > 299 {
-		fmt.Println("RequestAnnotationById(): Failed to delete annotation from the document page.")
+		fmt.Println("RequestAnnotationById(): Failed to request text annotation from the document page.")
 	} else {
 		fmt.Println("RequestAnnotationById(): annotation '" + annotation_id + "' successfully found '" + result.Annotation.Contents + "' in the document '" + document_name + "'.")
 	}
