9d822575a3ff2d922b6dc14c4b6bf60300e3632d
Dmitriy-Xawstov <dmytry.xawstov@aspose.com>
2025-07-24 23:36:18 +0300

    PDFCLOUD-4967: added snippets for Stamps

---
+++ uses_cases/stamps/stamps_append_text.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	asposepdfcloud "github.com/aspose-pdf-cloud/aspose-pdf-cloud-go/v25"
+)
+
+func AppendStampText(pdf_api *asposepdfcloud.PdfApiService, documentName string, outputDocument string, text_value string, remoteFolder string) {
+	uploadFile(pdf_api, documentName)
+
+	args := map[string]interface{}{
+		"folder": remoteFolder,
+	}
+
+	stamp := asposepdfcloud.TextStamp{
+		Background:          true,
+		HorizontalAlignment: asposepdfcloud.HorizontalAlignmentCenter,
+		VerticalAlignment:   asposepdfcloud.VerticalAlignmentCenter,
+		Opacity:             1,
+		Rotate:              asposepdfcloud.RotationNone,
+		RotateAngle:         30,
+		Zoom:                1,
+		TextAlignment:       asposepdfcloud.HorizontalAlignmentCenter,
+		Value:               text_value,
+		TextState:           &asposepdfcloud.TextState{FontSize: 14, FontStyle: asposepdfcloud.FontStylesBoldItalic, Font: "Arial", ForegroundColor: &asposepdfcloud.Color{A: 0xFF, R: 0xFF, G: 0x00, B: 0x00}},
+	}
+
+	_, httpResponse, err := pdf_api.PostDocumentTextStamps(documentName, []asposepdfcloud.TextStamp{stamp}, args)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else if httpResponse.StatusCode < 200 || httpResponse.StatusCode > 299 {
+		fmt.Println("appendStampText(): Failed to append text stamp to the document.")
+	} else {
+		fmt.Println("appendStampText(): text stamp '" + text_value + "' appended successfully to the document '" + documentName + "'.")
+		downloadFile(pdf_api, documentName, outputDocument, "add_text_stamp_")
+	}
+}
---
+++ uses_cases/stamps/stamps_helper.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+
+	asposepdfcloud "github.com/aspose-pdf-cloud/aspose-pdf-cloud-go/v25"
+)
+
+const (
+	REMOTE_FOLDER = "Your_Temp_Pdf_Cloud"
+	LOCAL_FOLDER  = "c:\\Samples"
+	PDF_DOCUMENT  = "sample.pdf"
+	PDF_OUTPUT    = "output_sample.pdf"
+
+	IMAGE_STAMP_FILE   = "sample.png"
+	PAGE_NUMBER        = 2
+	STAMP_TEXT         = "NEW TEXT STAMP"
+	IMAGE_STAMP_LLY    = 800
+	IMAGE_STAMP_WIDTH  = 24
+	IMAGE_STAMP_HEIGHT = 24
+
+	APP_SID = "****************" // Your Application SID
+	APP_KEY = "****************" // Your Application Key
+)
+
+func initPdfApi() *asposepdfcloud.PdfApiService {
+	// Initialize Credentials and create Pdf.Cloud service object
+	pdfApi := asposepdfcloud.NewPdfApiService(APP_SID, APP_KEY, "")
+	return pdfApi
+}
+
+func uploadFile(pdf_api *asposepdfcloud.PdfApiService, name string) {
+	args := map[string]interface{}{
+		"folder": REMOTE_FOLDER,
+	}
+	file, _ := os.Open(filepath.Join(LOCAL_FOLDER, name))
+	_, _, _ = pdf_api.UploadFile(filepath.Join(REMOTE_FOLDER, name), file, args)
+	fmt.Println("File '" + name + "' successfully uploaded!")
+}
+func downloadFile(pdf_api *asposepdfcloud.PdfApiService, name string, output_name string, output_prefix string) {
+	args := map[string]interface{}{
+		"folder": REMOTE_FOLDER,
+	}
+	result_data, _, _ := pdf_api.DownloadFile(path.Join(REMOTE_FOLDER, name), args)
+	fileName := path.Join(LOCAL_FOLDER, output_prefix+output_name)
+	f, _ := os.Create(fileName)
+	_, _ = f.Write(result_data)
+	fmt.Println("Result file'" + (output_prefix + output_name) + "' successfully downloaded!")
+}
